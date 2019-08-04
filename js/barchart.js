dcff4310281946230c433f0f194b58b16fb31166
neetughota <neetu.ghota@gmail.com>
2019-08-04 12:34:04 -0400

    Update barchart.js

--- js/barchart.js
+++ js/barchart.js
@@ -1,15 +1,25 @@
     function barchart(data , chartName, displayName ){  
   
-      const margin = { left: 10, right: 30, top: 5, bottom: 75 };
+      var margin = { top: 35, right: 0, bottom: 30, left: 40 };
 
-      const svg =  d3.select(chartName).append("svg").attr("width",350).attr("height",300);
+  var width = 350 - margin.left - margin.right;
+  var height = 500 - margin.top - margin.bottom;
+
+  var svg = d3.select(chartName).append("svg")
+      .attr("width", 350)
+      .attr("height", 500)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        
+
+   /*   const svg =  d3.select(chartName).append("svg").attr("width",350).attr("height",300);
       const width = svg.attr('width');
       const height = svg.attr('height');
       const innerWidth = width - margin.left - margin.right;
       const innerHeight = height - margin.top - margin.bottom; 
       const g = svg.append('g')
           .attr('transform', `translate(${margin.left},${margin.top})`);
- 
+ */
   var x = d3.scale.ordinal()
       .domain(data.map(function(d) { return d['name']; }))
       .rangeRoundBands([0, width], .1);
@@ -17,7 +27,6 @@
   var y = d3.scale.linear()
       .domain([0, d3.max(data, function(d) { return d['value']; }) * 1.1])
       .range([height, 0]);
-
 
 
   svg.append("g")
@@ -38,16 +47,22 @@
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("Value ($)");
+      .text("Stats");
 
   svg.selectAll("bar")
       .data(data)
     .enter().append("rect")
       .style("fill", "steelblue")
-      .attr("x", function(d) { return x(d.name); })
+      .attr("x", function(d) { 
+      return x(d.name); 
+  })
       .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.value); })
-      .attr("height", function(d) { return height - y(d.value); });
+      .attr("y", function(d) { 
+      return y(d.value); 
+  })
+      .attr("height", function(d) {
+      return height - y(d.value);
+  });
 
      
     };
