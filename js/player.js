fc71f35b8c7d5cc97867c37c66ca99bef2eb5ccb
neetughota <neetu.ghota@gmail.com>
2019-08-04 15:46:44 -0400

    Update player.js

--- js/player.js
+++ js/player.js
@@ -4,25 +4,13 @@ var showPlayerInfo = function(player){
 d3.json('Roster.json',function (data) {
   
   var filteredData =  data.players.filter(function(d) {  if( d.name == playerName) {return d};});
-    
-	var margin = {top: 40, right: 160, bottom: 35, left: 30}
-var width = 1300 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
+   
 	var div = document.getElementById("playerInfo");
 while(div.firstElementChild){
     div.removeChild(div.firstElementChild);
 }
 
 	
-d3.select("body").select("#playerInfo")  
- .append("svg")
- .attr("id","svg2")
- .attr("width", width + margin.left + margin.right)
- .attr("height", height + 100 + margin.top + margin.bottom)
- .append("g")
- .attr("transform", "translate(" + margin.left + "," + margin.top + ")");	
-
 	
   var html = '<div class="row" style="margin-right: 15px;margin-left: 15px;">'+
             '<div class="col" style="max-width: 30%;"> <img style="height:200px" src = "' + filteredData[0]["imgURL"] +'"></img> </div>'+
@@ -32,11 +20,9 @@ d3.select("body").select("#playerInfo")
       '<div class="col" > <div id="linechart"> </div> </div> ' +
       '</div>';
    
-  var svgid = document.getElementById("svg2")
-svgid.insertAdjacentHTML('afterbegin',html);
-
+ 
 	
-  // document.getElementById('playerInfo2').insertAdjacentHTML('afterbegin',html);
+   document.getElementById('playerInfo').insertAdjacentHTML('afterbegin',html);
    displayLineChart (player);
 	 var newData ={};
 	newData.FTA =  parseInt(player["FTA"]);
